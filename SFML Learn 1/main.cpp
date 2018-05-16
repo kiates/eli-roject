4123b74e41142f8f8b265f4b9111206d1a1a2839
Ajay <Ajay@AJAYLAPTOP>
2018-05-16 16:28:21 -0500

    -made the game fullscreen -working start and exit button on start screen -can now press escape to exit the program

--- SFML Learn 1/main.cpp
+++ SFML Learn 1/main.cpp
@@ -6,7 +6,7 @@
 #include "Level.h"
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(1280,720), "Window", sf::Style::Close | sf::Style::Titlebar); //defining size, name, and stles of the window
+	sf::RenderWindow window(sf::VideoMode(1280,720), "Window", sf::Style::Close | sf::Style::Titlebar | sf::Style::Fullscreen); //defining size, name, and stles of the window
 	//sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f)); // creates shape of 100 by 100
 	Player player(2, 7);
 	sf::View view(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(1280.0f, 720.0f));
@@ -14,11 +14,17 @@ int main() {
 	Level *level = new Level(&level1, dimX, dimY);
 	sf::Clock clock;
 	sf::Time refreshRate = sf::microseconds(1500);
+	sf::Mouse mouse;
+	sf::Vector2i mousePos = mouse.getPosition();
 
 	//start buttons
 	sf::RectangleShape startButton(sf::Vector2f(1006.0f, 156.0f));
 	sf::RectangleShape optoinButton(sf::Vector2f(1006.0f, 156.0f));
 	sf::RectangleShape exitButton(sf::Vector2f(1006.0f, 156.0f));
+
+	sf::IntRect startButtonInt(137, 64, 1006, 156);
+	sf::IntRect optoinButtonInt(137, 282, 1006, 156);
+	sf::IntRect exitButtonInt(137, 500, 1006, 156);
 
 	sf::Texture t_startButton;
 	sf::Texture t_optoinButton;
@@ -28,20 +34,28 @@ int main() {
 	sf::Texture t_optoinButtonHovered;
 	sf::Texture t_exitButtonHovered;
 
-	t_startButton.loadFromFile("Assets/Start Button.png");
-	t_optoinButton.loadFromFile("Assets/Option Button.png");
+	t_startButton.loadFromFile("Assets/Play Button.png");
+	t_optoinButton.loadFromFile("Assets/Options Button.png");
 	t_exitButton.loadFromFile("Assets/Exit Button.png");
 
-	t_startButtonHovered.loadFromFile("Assets/Start Button Hovered.png");
-	t_optoinButtonHovered.loadFromFile("Assets/Option Button Hovered.png");
+	t_startButtonHovered.loadFromFile("Assets/Play Button Hovered.png");
+	t_optoinButtonHovered.loadFromFile("Assets/Options Button Hovered.png");
 	t_exitButtonHovered.loadFromFile("Assets/Exit Button Hovered.png");
 
+	startButton.setPosition(sf::Vector2f(137.0f, 64.0f));
 	startButton.setTexture(&t_startButton);
-	startButton.setPosition(sf::Vector2f(100.0f, 100.0f));
 
-	bool startScreen = false;
+	optoinButton.setPosition(sf::Vector2f(137.0f, 282.0f));
+	optoinButton.setTexture(&t_optoinButton);
+
+	exitButton.setPosition(sf::Vector2f(137.0f, 500.0f));
+	exitButton.setTexture(&t_exitButton);
+	
+
+	bool startScreen = true;
 
 	while (window.isOpen()) { //main game loop
+		mousePos = mouse.getPosition();
 		clock.restart();
 		if (player.getPlayerHealth() <= 0.0f) {
 			delete level;
@@ -50,13 +64,18 @@ int main() {
 		}
 		sf::Event evnt; //defines event
 		while(window.pollEvent(evnt)) { //when the window gets an event
-			
+
+
 			if (evnt.type == evnt.Closed) { //when 'x' is pressed in the corner
 				window.close(); //closes the window
 			}
 
 		}
 		
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) {
+			window.close();
+		}
+
 		 // sets a view to follow the player
 		if (startScreen == false) {
 			view.setCenter(player.getPosition() + sf::Vector2f(player.getHeight() / 2, player.getWidth() / 2));
@@ -104,9 +123,37 @@ int main() {
 			window.clear(); // clears window
 		}
 		else {
+			if (startButtonInt.contains(mousePos)) {
+				startButton.setTexture(&t_startButtonHovered);
+				if (mouse.isButtonPressed(sf::Mouse::Button::Left)) {
+					startScreen = false;
+				}
+			}
+			else {
+				startButton.setTexture(&t_startButton);
+			}
+
+			if (optoinButtonInt.contains(mousePos)) {
+				optoinButton.setTexture(&t_optoinButtonHovered);
+			}
+			else {
+				optoinButton.setTexture(&t_optoinButton);
+			}
+
+			if (exitButtonInt.contains(mousePos)) {
+				exitButton.setTexture(&t_exitButtonHovered);
+				if (mouse.isButtonPressed(sf::Mouse::Button::Left)) {
+					window.close();
+				}
+			}
+			else {
+				exitButton.setTexture(&t_exitButton);
+			}
 			window.draw(startButton);
+			window.draw(optoinButton);
+			window.draw(exitButton);
 			window.display();
-			window.clear();
+			window.clear(sf::Color::Red);
 		}
 	}
 
